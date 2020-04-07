762d919d27d40a941d79b7edd94831898dd1f549
Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>
2020-04-08 01:38:20 +0200

    Add example using a RCL project hosted by a WebForms application.

---
+++ samples/mvc/AspNetWebForms/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AspNetWebForms.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>This is the default.aspx page executed by the ASP .NET infrastructure</p>
+            <ul>
+                <li><a href="OtherPage.aspx">Other ASPX page</a></li>
+                <li><a href="/mvc/">/mvc/ page (ASP .NET Core project) => Home/Index</a></li>
+                <li><a href="/mvc/home/redirection302about/">/mvc/home/redirection302about/ page (ASP .NET Core project) => Home/Redirection302</a></li>
+            </ul>
+        </div>
+    </form>
+</body>
+</html>
---
+++ samples/mvc/AspNetWebForms/Global.asax.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using AspNetMvcRazorClassLibrary;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AspNetWebForms
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            WebHost.CreateDefaultBuilder()
+                .UseAspNet(options =>
+                {
+                    options.Routes.Add("mvc");
+                })
+                .UseStartup<Startup>()
+                .Start();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
